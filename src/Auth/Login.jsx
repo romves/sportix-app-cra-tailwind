cfe9b7f810de38a7f9a09f6ea2e49a4e73d09ff1
Naufal <nofalrmz@gmail.com>
2023-03-11 12:11:53 +0700

    modified:   src/Auth/Login.jsx

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -41,41 +41,42 @@ const Login = () => {
         className=" h-screen object-cover rounded-r-2xl"
       />
       <div className="flex h-screen w-7/12 mx-10 items-center justify-end">
-        <InputCard className='w-[500px] h-[600px]'>
-          <h1 className="h1-semi">Masuk</h1>
-          <p className="mb-16 body-text">
-            Selamat datang kembali warrior! <br /> Lakukan pemesanan dan
-            dapatkan poin!
-          </p>
-          <p>{error.message}</p>
-          <form onSubmit={handleLogin} className="w-full flex flex-col">
-            <InputField
-              type="email"
-              name="email"
-              placeholder="Masukkan email"
-              onChange={(e) => setEmail(e.target.value)}
-              className='mb-2'
-            />
-            <InputField
-              type="password"
-              name="password"
-              placeholder="Masukkan kata sandi"
-              onChange={(e) => setPassword(e.target.value)}
-              width={"440px"}
-            />
-            <p className="text-end text-xs text-primary-500">
-              Lupa kata sandi?
+        <InputCard className="w-[500px] min-w-[500px] h-[600px] ">
+          <div className=" min-w-[440px]">
+            <h1 className="h1-semi">Masuk</h1>
+            <p className="mb-16 body-text">
+              Selamat datang kembali warrior! <br /> Lakukan pemesanan dan
+              dapatkan poin!
             </p>
-            <div className="flex flex-col items-center pt-[85px] text-white">
-              <Button className='my-4'>Masuk</Button>
-              <div className="flex m-auto p-1 border border-black rounded-lg">
-                <p className="text-primary-500 text-center body-md-norm">
-                  <span className="text-black">Belum punya akun?</span>{" "}
-                  <Link to="/signup">Daftar</Link>
-                </p>
+            <p>{error.message}</p>
+            <form onSubmit={handleLogin} className="w-full flex flex-col">
+              <InputField
+                type="email"
+                name="email"
+                placeholder="Masukkan email"
+                onChange={(e) => setEmail(e.target.value)}
+                className="mb-2"
+              />
+              <InputField
+                type="password"
+                name="password"
+                placeholder="Masukkan kata sandi"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <p className="text-end text-xs text-primary-500">
+                Lupa kata sandi?
+              </p>
+              <div className="flex flex-col items-center pt-[85px] text-white">
+                <Button className="my-4">Masuk</Button>
+                <div className="flex m-auto p-1 border border-black rounded-lg">
+                  <p className="text-primary-500 text-center body-md-norm">
+                    <span className="text-black">Belum punya akun?</span>{" "}
+                    <Link to="/signup">Daftar</Link>
+                  </p>
+                </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </InputCard>
       </div>
     </div>
